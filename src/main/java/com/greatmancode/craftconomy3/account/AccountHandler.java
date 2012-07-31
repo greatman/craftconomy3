a707431202ff364c3ceecc6d577c0239245abbe1
William Lebel <lebelwilliam@gmail.com>
2012-07-31 13:06:02 -0400

    Finished Delete command. Bumped SimpleSave version Added a exist() to AccountHandler.

--- src/main/java/com/greatmancode/craftconomy3/account/AccountHandler.java
+++ src/main/java/com/greatmancode/craftconomy3/account/AccountHandler.java
@@ -2,6 +2,9 @@ package com.greatmancode.craftconomy3.account;
 
 import java.util.HashMap;
 
+import com.greatmancode.craftconomy3.Common;
+import com.greatmancode.craftconomy3.database.tables.AccountTable;
+
 public class AccountHandler {
 	public HashMap<String, Account> accountList = new HashMap<String, Account>();
 
@@ -15,6 +18,14 @@ public class AccountHandler {
 			
 		}
 		return account;
+	}
+	
+	/**
+	 * Check if a account exist in the database.
+	 * @param name The name to check
+	 */
+	public boolean exist(String name) {
+		return Common.getInstance().getDatabaseManager().getDatabase().select(AccountTable.class).where().contains("name", name).execute().findOne() != null;
 	}
 	
 	
--- src/main/java/com/greatmancode/craftconomy3/commands/money/DeleteCommand.java
+++ src/main/java/com/greatmancode/craftconomy3/commands/money/DeleteCommand.java
@@ -2,13 +2,21 @@ package com.greatmancode.craftconomy3.commands.money;
 
 import com.greatmancode.craftconomy3.Common;
 import com.greatmancode.craftconomy3.commands.CraftconomyCommand;
+import com.greatmancode.craftconomy3.database.tables.AccountTable;
+import com.greatmancode.craftconomy3.database.tables.BalanceTable;
 
 public class DeleteCommand implements CraftconomyCommand {
 
 	@Override
 	public void execute(String sender, String[] args) {
-		// TODO Auto-generated method stub
-
+		if (Common.getInstance().getAccountHandler().exist(args[0])) {
+			AccountTable account = Common.getInstance().getDatabaseManager().getDatabase().select(AccountTable.class).where().contains("name", args[0]).execute().findOne();
+			Common.getInstance().getDatabaseManager().getDatabase().remove(Common.getInstance().getDatabaseManager().getDatabase().select(BalanceTable.class).where().contains("username_id", account.id).execute().find());
+			Common.getInstance().getDatabaseManager().getDatabase().remove(account);
+			Common.getInstance().getServerCaller().sendMessage(sender, "{{GREEN}}The account {{WHITE}}" + args[0] + " {{GREEN}}has been deleted!");
+		} else {
+			Common.getInstance().getServerCaller().sendMessage(sender, "{{RED}}This account doesn't exist!");
+		}
 	}
 
 	@Override
