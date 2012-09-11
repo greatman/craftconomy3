b600cc97267292d85f55c2bcfb3806225f73621e
Guillaume Derval <guillaume@itself.be>
2012-09-11 13:24:16 +0200

    Some work on /money top

--- src/main/java/com/greatmancode/craftconomy3/commands/money/TopCommand.java
+++ src/main/java/com/greatmancode/craftconomy3/commands/money/TopCommand.java
@@ -18,16 +18,106 @@
  */
 package com.greatmancode.craftconomy3.commands.money;
 
+import com.alta189.simplesave.query.OrderQuery;
+import com.alta189.simplesave.query.QueryResult;
+import com.alta189.simplesave.query.SelectQuery;
 import com.greatmancode.craftconomy3.Common;
 import com.greatmancode.craftconomy3.commands.CraftconomyCommand;
+import com.greatmancode.craftconomy3.currency.Currency;
+import com.greatmancode.craftconomy3.currency.CurrencyManager;
+import com.greatmancode.craftconomy3.database.tables.AccountTable;
+import com.greatmancode.craftconomy3.database.tables.BalanceTable;
+
+class TopCommandThread implements Runnable
+{
+	class TopCommandThreadEnd implements Runnable
+	{
+		String sender;
+		String ret;
+		
+		public TopCommandThreadEnd(String sender, String ret)
+		{
+			this.sender = sender;
+			this.ret = ret;
+		}
+		
+		@Override
+		public void run()
+		{
+			Common.getInstance().getServerCaller().sendMessage(sender, ret);
+		}
+	}
+	
+	private String sender;
+	private int page;
+	private String world;
+	private int currency;
+	
+	public TopCommandThread(String sender, int page, String world, int currency)
+	{
+		this.sender = sender;
+		this.page = page;
+		this.world = world;
+		this.currency = currency;
+	}
+	
+	@Override
+	public void run()
+	{
+		String ret = "{{DARK_GREEN}} money top | page {{WHITE}}"+page+"{{DARK_GREEN}} | world {{WHITE}}"+world+"\n";
+		SelectQuery<BalanceTable> balanceQuery = Common.getInstance().getDatabaseManager().getDatabase().select(BalanceTable.class);
+		balanceQuery.where().equal("worldName", world).and().equal("currency_id", currency);
+		balanceQuery.order().getPairs().add(new OrderQuery.OrderPair("balance", OrderQuery.Order.DESC));
+		balanceQuery.limit().setLimit((page-1)*50, 50);
+		QueryResult<BalanceTable> balanceResult = balanceQuery.execute();
+		for(int i = 0; i < balanceResult.find().size(); i++)
+		{
+			BalanceTable r = balanceResult.find().get(i);
+			
+			// Is it better to do 50 query or to get ALL the username-id pairs?
+			// I choose the first solution. This is done async and will save lot of memory on large server with lots of players/account.
+			
+			AccountTable usernameResult = Common.getInstance().getDatabaseManager().getDatabase().select(AccountTable.class).where().equal("id", r.username_id).execute().findOne();
+			String username = "ERROR";
+			if(usernameResult != null)
+				username = usernameResult.name;
+			ret+= ""+ ((page-1)*50+i+1) + ": {{DARK_GREEN}}"+username+" {{WHITE}}"+r.balance+"\n";
+		}
+		
+		Common.getInstance().getServerCaller().delay(new TopCommandThreadEnd(sender, ret), 0, false);
+	}
+}
 
 public class TopCommand implements CraftconomyCommand {
 
 	@Override
 	public void execute(String sender, String[] args) {
-
-		// TODO: Need alta to add a orderQuery
-		Common.getInstance().getServerCaller().sendMessage(sender, "{{DARK_RED}}Command not yet implemented.");
+		int page = 1;
+		Currency currency = Common.getInstance().getCurrencyManager().getCurrency(CurrencyManager.defaultCurrencyID);
+		if (Common.getInstance().getCurrencyManager().getCurrency(args[0]) != null) {
+			currency = Common.getInstance().getCurrencyManager().getCurrency(args[1]);
+		} else {
+			Common.getInstance().getServerCaller().sendMessage(sender, "{{DARK_RED}}That currency doesn't exist!");
+			return;
+		}
+		
+		if(args.length > 1) {
+			try {
+				page = Integer.parseInt(args[1]);
+				if(page < 1)
+					page = 1;
+			} catch(NumberFormatException e) {
+				Common.getInstance().getServerCaller().sendMessage(sender, "{{DARK_RED}}Invalid page!");
+				return;
+			}
+		}
+		
+		String world = "any";
+		if (args.length > 2) {
+			world = args[2];
+		}
+		
+		Common.getInstance().getServerCaller().delay(new TopCommandThread(sender, page, world, currency.getDatabaseID()), 0, false);
 	}
 
 	@Override
@@ -37,12 +127,12 @@ public class TopCommand implements CraftconomyCommand {
 
 	@Override
 	public String help() {
-		return "/money top <Currency> [World] - Shows the top list";
+		return "/money top <Currency> [Page] [World] - Shows the top list";
 	}
 
 	@Override
 	public int maxArgs() {
-		return 2;
+		return 3;
 	}
 
 	@Override
