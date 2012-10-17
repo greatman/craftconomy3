6921d26dfec75467fc249394d23abef68a77729a
William Lebel <lebelwilliam@gmail.com>
2012-10-17 00:07:30 -0400

    more unit tests.

--- src/test/java/com/greatmancode/craftconomy3/commands/TestCommands.java
+++ src/test/java/com/greatmancode/craftconomy3/commands/TestCommands.java
@@ -9,15 +9,20 @@ import junit.framework.TestCase;
 public class TestCommands extends TestCase {
 
 	public void testCommands() {
-		Reflections reflections = new Reflections("com.greatmancode.craftconomy3.commands.bank");
+		Reflections reflections = new Reflections("com.greatmancode.craftconomy3.commands");
 		Iterator<Class<? extends CraftconomyCommand>> allClasses = reflections.getSubTypesOf(CraftconomyCommand.class).iterator();
 		while (allClasses.hasNext()) {
 			Class<? extends CraftconomyCommand> clazz = allClasses.next();
 			try {
 				CraftconomyCommand instance = clazz.newInstance();
-				System.out.println("Testing : " + instance.help());
 				if (!(instance.help() instanceof String)) {
-					fail("Fail!");
+					fail("Help is null for: " + clazz.getName());
+				}
+				if (instance.maxArgs() < 0) {
+					fail("Fail maxArgs for class: " + clazz.getName());
+				}
+				if (instance.minArgs() < 0) {
+					fail("Fail minArgs for class: " + clazz.getName());
 				}
 			} catch (InstantiationException e) {
 				fail(e.getMessage());
