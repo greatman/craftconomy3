b1ecb7c0a40dd1467378ced3183af98001b84869
William Lebel <lebelwilliam@gmail.com>
2013-08-04 10:45:50 -0400

    Rewrite Essentials importer. No more red line!

--- src/main/java/com/greatmancode/craftconomy3/converter/converters/Essentials.java
+++ src/main/java/com/greatmancode/craftconomy3/converter/converters/Essentials.java
@@ -18,8 +18,12 @@
  */
 package com.greatmancode.craftconomy3.converter.converters;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,26 +63,29 @@ public class Essentials extends Converter {
 		List<User> userList = new ArrayList<User>();
 
 		int i = 0;
+        String line;
 		for (File account : accounts){
-			Config config = Common.getInstance().getConfigurationManager().loadFile(accountsFolder, account.getName());
-			String name = account.getName().replace(".yml", "");
-
-			try {
-				double money = Double.parseDouble(config.getString("money"));
-				if (money != -1){
-					userList.add(new User(name, money));
-				}
-			} catch (NumberFormatException e) {
-				Common.getInstance().getServerCaller().getPlayerCaller().sendMessage(sender, "{{DARK_RED}}Account {{WHITE}}" + name + " {{DARK_RED}} didin't import due to a error!");
-			} catch (NullPointerException e) {
-				Common.getInstance().getServerCaller().getPlayerCaller().sendMessage(sender, "{{DARK_RED}}Account {{WHITE}}" + name + " {{DARK_RED}} didin't import due to a error!");
-			}
-
-
-			if (i % 10 == 0) {
-				Common.getInstance().getServerCaller().getPlayerCaller().sendMessage(sender, i + " {{DARK_GREEN}}accounts loaded.");
-			}
-			i++;
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(account));
+                while ((line = reader.readLine()) != null) {
+                //money: '0.0'
+                    if (line.contains("money")) {
+                        String value = line.replace("money: '", "");
+                        value = value.substring(0, value.length() - 1);
+                        double money = Double.parseDouble(value);
+                        String name = account.getName().replace(".yml", "");
+                        userList.add(new User(name, money));
+                    }
+                }
+                if (i % 10 == 0) {
+                    Common.getInstance().getServerCaller().getPlayerCaller().sendMessage(sender, i + " {{DARK_GREEN}}accounts loaded.");
+                }
+                i++;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 		}
 		addAccountToString(userList);
 		addBalance(sender, userList);
